11e76b4ee10f1acaf94237a26f063ea9261a512a
22victoryy <dk90801@gmail.com>
2024-07-07 03:06:17 -0400

    First commit

---
+++ bin/aws_code_pipeline.ts
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { AwsCodePipelineStack} from '../lib/aws_code_pipeline-stack';
+
+const app = new cdk.App();
+
+new AwsCodePipelineStack(app, 'AwsCodePipelineStack', {
+  env: {
+    account: '482505494228',
+    region: 'us-east-1',
+  }
+});
+
+app.synth();
--- test/aws_code_pipeline.test.ts
+++ test/aws_code_pipeline.test.ts
@@ -1,9 +1,9 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as AwsCodePipeline from '../lib/aws code_pipeline-stack';
+// import * as AwsCodePipeline from '../lib/aws_code_pipeline-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/aws code_pipeline-stack.ts
+// example resource in lib/aws_code_pipeline-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
